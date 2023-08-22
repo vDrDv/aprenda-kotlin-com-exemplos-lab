e1f76b1447271b4923c702157b59fcd8babc07c1
VANDER VIANA <103910413+vDrDv@users.noreply.github.com>
2023-08-22 00:01:26 -0300

    Abstração de simulação de 3 matriculas, para resolver o desafio

--- desafio.kt
+++ desafio.kt
@@ -1,5 +1,3 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
@@ -11,11 +9,23 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.hashCode()} matriculado na Formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+    
+    val conteudo1 = ConteudoEducacional("Introdução à Programação")
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados")
+    val conteudo3 = ConteudoEducacional("Banco de Dados")
+    
+    val formacao1 = Formacao("Desenvolvimento Web", listOf(conteudo1, conteudo2))
+    val formacao2 = Formacao("Ciência de Dados", listOf(conteudo1, conteudo3))
+    
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao2.matricular(usuario1)
 }
